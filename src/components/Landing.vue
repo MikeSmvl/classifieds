cbcdf71aa2624142785f56daf487b5970346908e
Mikael Samvelian <mikael.samvelian@hotmail.com>
2017-09-16 17:31:35 -0400

    Add routing
    Made a couple of components that will act as different pages in the process of visiting the website, creating an account and logging in. (Landing/Signup/Signin/Home/.vue)
    Changed app.vue and index.js to properly route said files and to export their contents inside the main tag on our application view file. (App.vue)
    All of the changes were made following the tutorial found on this website: https://medium.com/@oleg.agapov/basic-single-page-application-using-vue-js-and-firebase-part-1-9e4c0c11a228.

---
+++ src/components/Landing.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-layout row>
+    <v-flex>
+      <h3>Landing page</h3>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+  export default {}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+
+const routerArray = [
+  { path: '/', component: 'Landing' },
+  { path: '/signin', component: 'Signin' },
+  { path: '/signup', component: 'Signup' },
+  { path: '/home', component: 'Home' }
+]
+
+const routes = routerArray.map(route => {
+  return {
+    path: route.path,
+    component: () => import(`@/components/${route.component}.vue`)
+  }
+})
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+  mode: 'history',
+  routes
 })
