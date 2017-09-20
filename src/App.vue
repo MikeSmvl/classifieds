bc25ba40ca925996ba9a4bcad301c2eea16a89cf
The Phi Nguyen <g.poseidon@hotmail.com>
2017-09-20 15:45:26 -0400

    Menu added
    I have defined a way to go back to the main menu every time the user
    click on the “classified exchange app”. Once click on it, it will
    return to the main menu. Nonetheless, when the user shrink the window,
    the toolbar will show a small icon that will list the content. The idea
    behind that is we have a workable menu.
    
    todo: add more content to it

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
   <v-app>
+
+
     <v-navigation-drawer temporary v-model="sidebar">
+      <v-list>
+        <v-list-tile
+          v-for="item in menuItems" :key="item.title" :to="item.path">
+          <v-list-tile-action>
+            <v-icon>
+              {{item.icon}}
+            </v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            {{item.title}}
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
+
+
     <v-toolbar fixed>
       <span class="hidden-sm-and-up">
         <v-toolbar-side-icon @click.stop="sidebar = !sidebar">
         </v-toolbar-side-icon>
       </span>
-      <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+      <v-toolbar-title>
+      <router-link to="/" tag="span" style="cursor: pointer">
+        {{appTitle}}
+      </router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          flat
+          v-for="item in MenuItems" :key="item.title" :to="item.path">
+          <v-icon left dark>
+            {{item.icon}}
+          </v-icon>
+          {{item.title}}
+          </v-btn>
+    </v-toolbar-items>
     </v-toolbar>
+
 
     <main>
       <v-container fluid>
@@ -19,12 +51,28 @@
       </v-container>
     </main>
 
+
   </v-app>
 </template>
+
+
 <script>
+  import VListTile from 'vuetify/src/components/VList/VListTile'
+  import VListTileAction from 'vuetify/src/components/VList/VListTileAction'
+  import VIcon from '../node_modules/vuetify/src/components/VIcon/VIcon.vue'
+  import VToolbar from '../node_modules/vuetify/src/components/VToolbar/VToolbar.vue'
+  import VBtn from '../node_modules/vuetify/src/components/VBtn/VBtn.vue'
+
   export default {
+    components: {
+      VBtn,
+      VToolbar,
+      VIcon,
+      VListTileAction,
+      VListTile},
     data () {
       return {
+        appTitle: 'Classified Exchange App',
         sidebar: false,
         menuItems: [
           { title: 'Home', path: '/home', icon: 'home' },
@@ -32,14 +80,11 @@
           { title: 'Sign In', path: '/signin', icon: 'lock_open' }
         ]
       }
-    },
-    computed: {
-      appTitle () {
-        return this.$store.getters.appTitle
-      }
     }
   }
 </script>
+
+
 <style lang="stylus">
   @import './stylus/main'
 </style>
