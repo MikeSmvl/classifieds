f748f4915786e6a0525e1627a4956545186b93c9
darkheros12 <g.poseidon@hotmail.com>
2017-10-19 01:09:13 -0400

    Search Algorithms
    I have a search algorithms. The problem is I can't find the array of the
    list of postedItems because I want to search through that list.  I put
    the code in comment so it does not comflict with the current project.

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -3,7 +3,34 @@
         <v-flex sm6 mt-5>
             <v-card class="pl-5 pr-5 pt-1 pb-4">
                 <v-text-field label="Search..." single-line append-icon="search" light hide-details></v-text-field>
+                <input type = "text" v-model="search"/>
             </v-card>
         </v-flex>
     </v-layout>
 </template>
+
+<!--<script>
+  import AdList from '../components/AdList.vue'
+  export default {
+    data () {
+      return {
+        adListFiltered: [],
+        search: ''
+      }
+    },
+    methods: {
+    },
+    created () {
+      this.$http.get('http://crashcode.ca/home').then(function (data) {
+        this.filtedAdList = data.body.slice(0, 10)
+      })
+    },
+    computed: {
+      theListfilteredAdlist: function () {
+        return this.adListFiltered.filter((this.$store.getters.getAdList) => {
+          return this.$store.getters.getAdList.title.match(this.search)
+        })
+      }
+    }
+  }
+</script>-->
