8dadd3db3d476218e7a71c072692fa680a4efd1b
Mikael Samvelian <mikael.samvelian@hotmail.com>
2017-10-28 21:36:37 -0400

    Update category/searchbar
    I changed the grid layout of the searchbar and changed the category.json file to have the "select a category" label show up inside instead of in the corner.

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,48 +1,48 @@
 <template>
-    <v-layout row justify-center mb-0>
-        <v-flex sm6 mt-5>
-            <v-card class="pl-5 pr-5 pt-1 pb-4">
-                <v-text-field @keyup.enter.native="search" v-model="value" label="Search..." single-line append-icon="search" light hide-details></v-text-field>
-                <v-select @change="onCategoryChange"
-	              v-bind:items="items"
-	              v-model="keyCategory"
-	              label="Select a category"
-	              class="input-group--focused"
-	              item-text="name"
-	              item-value="key"
-	            ></v-select>			   
-	            <v-select v-if="subItems.length > 0" 
-	              v-bind:items="subItems"
-	              v-model="keySubCategory"
-	              label="Select a sub category"
-	              class="input-group--focused"
-	              item-text="name"
-	              item-value="key"
-	            ></v-select>			         
-            </v-card>
-        </v-flex>
-    </v-layout>
+  <v-layout row>
+    <v-flex xs12 sm8 offset-sm2>
+      <v-card class="pl-5 pr-5 pt-1 pb-4">
+        <v-text-field @keyup.enter.native="search" v-model="value" label="Search..." single-line append-icon="search" hide-details></v-text-field>
+        <v-select @change="onCategoryChange"
+                  v-bind:items="items"
+                  v-model="keyCategory"
+                  label=""
+                  class="input-group--focused"
+                  item-text="name"
+                  item-value="key"
+        ></v-select>
+        <v-select v-if="subItems.length > 0"
+                  v-bind:items="subItems"
+                  v-model="keySubCategory"
+                  label=""
+                  class="input-group--focused"
+                  item-text="name"
+                  item-value="key"
+        ></v-select>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
-export default {
-  data () {
-    return {
-      items: this.$store.getters.getCategoryList,
-      keyCategory: '',
-      keySubCategory: '',
-      subItems: '',
-      searchInput: ''
-    }
-  },
-  methods: {
-    onCategoryChange (value) {
-      this.$store.dispatch('filterSubCategory', {keyCategory: value})
-      this.subItems = this.$store.getters.getSubCategoryList
-      this.keySubCategory = ''
+  export default {
+    data () {
+      return {
+        items: this.$store.getters.getCategoryList,
+        keyCategory: '',
+        keySubCategory: '',
+        subItems: '',
+        searchInput: ''
+      }
     },
-    search () {
-      this.$store.dispatch('search', {searchInput: this.value})
+    methods: {
+      onCategoryChange (value) {
+        this.$store.dispatch('filterSubCategory', {keyCategory: value})
+        this.subItems = this.$store.getters.getSubCategoryList
+        this.keySubCategory = ''
+      },
+      search () {
+        this.$store.dispatch('search', {searchInput: this.value})
+      }
     }
   }
-}
 </script>
